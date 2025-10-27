cb5f86eb130dd002d43286cf21ab7469b210ba46
AmahleDamane <ST10480568@vcconnect.edu.za>
2025-10-27 13:14:43 +0200

    added screens

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Screen1 from './screens/Screen1';
+import Screen2 from './screens/Screen2';
+import Screen3 from './screens/Screen3';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor:'green',
+        tabBarInactiveTintColor:'grey'
+        
+
+      }}
+      >
+
+        
+        <Tab.Screen 
+        name="Screen 1" 
+        component={Screen1} 
+        options= {{tabBarIcon: ({color,size})=> 
+        <FontAwesome6 name="gun" size={24} color="blue" />}}/>
+
+        <Tab.Screen 
+        name="Screen 2" 
+        component={Screen2} />
+
+        <Tab.Screen 
+        name="Screen 3" 
+        component={Screen3} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
